6af05f4ff4b81c6a119e9380fb2e23313bb0829b
Juan Martin Silva <juanmartinsilva74@gmail.com>
2023-08-01 00:14:55 -0300

    fix link of route

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -27,7 +27,7 @@ const List<MenuItems> appMenuItems = [
     MenuItems(
         title: 'Tarjetas', 
         subtitle: 'Un contenedor estilizado', 
-        link: '/card', 
+        link: '/cards', 
         icon: Icons.credit_card,
     ),
 
